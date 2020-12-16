7659c3cd96e739a0366a90b7a9e10ba914270d4b
guxianwang <guxw@njzhyl.cn>
2020-12-16 15:04:20 +0800

    项目模板CLI-1

--- src/main.js
+++ src/main.js
@@ -3,15 +3,14 @@ import App from './App.vue';
 import router from './router/index';
 import store from './store';
 import './styles/index.scss';
+import 'normalize.css/normalize.css';
 
-require('./mock'); // 引入mockjs
+if (process.env.NODE_ENV !== 'production') {
+    require('./mock'); // 引入mockjs
+}
 
 // 引入icon
 import './icons';
-
-import 'normalize.css/normalize.css';
-
-console.log(process.env.VUE_APP_URL)
 
 Vue.config.productionTip = false;
 
--- vue.config.js
+++ vue.config.js
@@ -6,10 +6,20 @@ function resolve(dir) {
 
 module.exports = {
     outputDir: process.env.NODE_ENV === "development" ? 'devdist' : 'dist', // 不同的环境打不同包名
+    publicPath: './',
+    productionSourceMap: false,
+    chainWebpack: (config) => {
+        config.plugin('define').tap((definitions) => {
+            const args = definitions[0]['process.env'];
+            args.VUE_APP_APP_NAME = JSON.stringify(require('./package.json').name);
+            args.VUE_APP_APP_VERSION = JSON.stringify(require('./package.json').version);
+            return definitions;
+        });
+    },
     configureWebpack: {
         resolve: {
             // 添加别名
-            alias: { 
+            alias: {
                 'assets': resolve('src/assets'),
                 'components': resolve('src/components'),
                 'views': resolve('src/views')
@@ -17,5 +27,7 @@ module.exports = {
         }
     },
     // 关闭eslint
-    lintOnSave: false
+    // lintOnSave: false
+    /* 必须对下面这些包也通过babel编译，因为其发布代码语法为es6，不兼容旧版浏览器 */
+    // transpileDependencies: [],
 };
