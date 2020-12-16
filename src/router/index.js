97b01f58ac29786332b350eeb3150b3824702d82
guxianwang <guxw@njzhyl.cn>
2020-12-16 17:52:10 +0800

    router add noprogress

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import NProgress from 'nprogress';
 
 Vue.use(Router);
 
@@ -12,6 +13,15 @@ export const routes = [
         component: Layout,
     },
 ];
+
+router.beforeEach(() => {
+    NProgress.start();
+});
+
+router.afterEach(() => {
+    NProgress.done();
+});
+
 
 export default new Router({
     mode: 'history',
