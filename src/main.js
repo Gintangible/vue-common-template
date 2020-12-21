14bb0a42f424c5bfbb50dfd82ed8eca967c2a134
guxianwang <guxw@njzhyl.cn>
2020-12-21 15:46:50 +0800

    update package.json

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue';
+import 'amfe-flexible';
+import 'normalize.css/normalize.css';
+import '@/styles/scrollbar.css';
 import App from './App.vue';
 import router from './router/index';
 import store from './store';
-import 'normalize.css/normalize.css';
 
 if (process.env.NODE_ENV !== 'production') {
-    require('./mock'); // 引入mockjs
-    const eruda = require('eruda');
-    eruda.init();
+  require('./mock'); // 引入mockjs
+  const eruda = require('eruda');
+  eruda.init();
 }
 
 // 引入icon
@@ -16,7 +18,7 @@ import './icons';
 Vue.config.productionTip = false;
 
 new Vue({
-    router,
-    store,
-    render: h => h(App)
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app');
