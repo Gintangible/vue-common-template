a3157a7969e964ecd3ba766952fd983495bcdba7
guxianwang <guxw@njzhyl.cn>
2021-01-25 11:36:18 +0800

    添加login 判断

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
+import store from '@/store';
 
 Vue.use(Router);
 
@@ -29,10 +30,19 @@ const router = new Router({
   routes,
 });
 
+const WHITE_LIST = ['Login'];
+
 router.beforeEach((to, from, next) => {
   NProgress.start();
-  if (next) {
+  if (to.meta.title) {
+    document.title = to.meta.title;
+  }
+  if (WHITE_LIST.includes(to.name) || store.state.user.name) {
     next();
+  } else {
+    next({
+      name: 'Login',
+    });
   }
 });
 
